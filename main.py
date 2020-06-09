8e46f71293d3885041f580156230dcdc86a555fc
Dominik <dominik.kummer@odist.at>
2020-06-09 16:27:31 +0200

    started with pyqt5 from scratch

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 import sys
+import matplotlib
+matplotlib.use('Qt5Agg')
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
+from matplotlib.backend_bases import MouseButton
+from matplotlib.figure import Figure
 from configparser import ConfigParser
 from PyQt5 import QtCore
-from PyQt5.QtGui import QCursor
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout, QGroupBox, QWidget, QVBoxLayout
 
+
+class MplCanvas(FigureCanvasQTAgg):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
+
+        self.dragging = False
+        self.drag_start_pos = None
+
+        self.mpl_connect('motion_notify_event', self.cursor_move)
+        self.mpl_connect('button_press_event', self.button_press)
+        self.mpl_connect('button_release_event', self.button_release)
+
+    def cursor_move(self, event):
+        if self.dragging:
+            print('From:',self.drag_start_pos,'To:', (event.xdata, event.ydata))
+
+    def button_press(self, event):
+        if event.button.value == MouseButton.LEFT:
+            self.drag_start_pos = (event.xdata, event.ydata)
+            self.dragging = True
+
+    def button_release(self, event):
+        if event.button.value == MouseButton.LEFT:
+            self.dragging = False
+
+    def plot_current(self, datax, datay):
+        self.axes.plot(datax,datay)
+
+class HeadWidget(QWidget):
+    def __init__(self, parent):
+        self.parent = parent
+        super(HeadWidget, self).__init__(self.parent)
 
 class GraphMainWindow(QMainWindow):
     def __init__(self,cfg):
@@ -13,21 +52,34 @@ class GraphMainWindow(QMainWindow):
 
     def configUI(self):
         self.setWindowTitle(self.cfg['GUI']['title'])
-        self.resize(self.cfg['GUI'].getint('width'),self.cfg['GUI'].getint('heigth'))
+        if self.cfg['GUI'].getboolean('always_on_top'):
+            self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+        self.setGeometry(self.cfg['GUI'].getint('offset_x'),self.cfg['GUI'].getint('offset_y'), self.cfg['GUI'].getint('width'),self.cfg['GUI'].getint('heigth'))
+        if self.cfg['GUI'].getboolean('fullscreen'):
+            self.showFullScreen()
 
     def initUI(self):
         self.configUI()
-        layout = QGridLayout()
-        layout
-        self.btn_save = QPushButton(self.cfg['STRINGS']['btn_save'], self)
-        self.btn_save.show()
-        self.btn_load = QPushButton(self.cfg['STRINGS']['btn_load'], self)
-        self.btn_load.show()
-        self.btn_submit = QPushButton(self.cfg['STRINGS']['btn_submit'], self)
-        self.btn_submit.show()
-        self.btn_compare = QPushButton(self.cfg['STRINGS']['btn_compare'], self)
-        self.btn_compare.show()
 
+        self.graph = MplCanvas(self, width=5, height=4, dpi=100)
+        self.graph.plot_current([0,1,2,3,4], [10,1,20,3,40])
+
+        # Create toolbar, passing canvas as first parament, parent (self, the MainWindow) as second.
+        toolbar = NavigationToolbar2QT(self.graph, self)
+
+        head = HeadWidget(self)
+
+        layout = QVBoxLayout()
+
+        layout.addWidget(head)
+        layout.addWidget(self.graph)
+        layout.addWidget(toolbar)
+
+
+        # Create a placeholder widget to hold our toolbar and canvas.
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
         self.show()
 
 
@@ -35,7 +87,7 @@ class GraphMainWindow(QMainWindow):
 
 if __name__ == '__main__':
     config = ConfigParser()
-    config.read('config.ini')
+    config.read('config.ini',encoding='utf-8')
     app = QApplication(sys.argv)
     gui = GraphMainWindow(config)
     sys.exit(app.exec_())
