57675f8ede3ac7e853e54b641aa4ff50e572e499
Dominik <dominik.kummer@odist.at>
2020-06-10 15:02:59 +0200

    started with pyqt5 from scratch

--- MainWindow.py
+++ MainWindow.py
@@ -14,52 +14,198 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1392, 808)
+        MainWindow.resize(1517, 1082)
+        MainWindow.setMinimumSize(QtCore.QSize(800, 600))
+        MainWindow.setStyleSheet("QPushButton {\n"
+"  background-color: #d9ddff;\n"
+"  border: 1px solid #32414B;\n"
+"  border-radius: 5px;\n"
+"  padding: 3px;\n"
+"  color: #000000\n"
+"}\n"
+"\n"
+"\n"
+"QPushButton:pressed {\n"
+"  background-color: #6270f5;\n"
+"}\n"
+"\n"
+"QPushButton:pressed:hover {\n"
+"   background-color: #6270f5;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"  border: 1px solid #148CD2;\n"
+"  background-color: #b8bfff;\n"
+"}\n"
+"\n"
+"QPushButton:selected {\n"
+"  background: #1464A0;\n"
+"  color: #32414B;\n"
+"}\n"
+"\n"
+"QPushButton:focus {\n"
+"  border: 1px solid #1464A0;\n"
+"}\n"
+"\n"
+"QPushButton:checked {\n"
+"  background-color: #6270f5;\n"
+"}\n"
+"\n"
+"\n"
+"\n"
+"\n"
+"\n"
+"\n"
+"QComboBox {\n"
+"  background-color: #d9ddff;\n"
+"  border: 1px solid #32414B;\n"
+"  border-radius: 5px;\n"
+"  selection-background-color: #1464A0;\n"
+"}\n"
+"QComboBox QAbstractItemView {\n"
+"  border: 1px solid #32414B;\n"
+"  border-radius: 2px;\n"
+"  background-color: #d9ddff;\n"
+"  selection-background-color: #1464A0;\n"
+"}")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
+        self.centralwidget.setSizePolicy(sizePolicy)
+        self.centralwidget.setSizeIncrement(QtCore.QSize(1, 1))
         self.centralwidget.setObjectName("centralwidget")
-        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 129, 1401, 631))
-        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
-        self.mplLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
-        self.mplLayout.setSizeConstraint(QtWidgets.QLayout.SetMaximumSize)
-        self.mplLayout.setContentsMargins(0, 0, 0, 0)
-        self.mplLayout.setObjectName("mplLayout")
-        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox.setGeometry(QtCore.QRect(20, 10, 161, 81))
-        self.groupBox.setObjectName("groupBox")
-        self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.groupBox)
-        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(10, 20, 141, 61))
-        self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
-        self.layout_btn_grp_01 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
-        self.layout_btn_grp_01.setContentsMargins(0, 0, 0, 0)
-        self.layout_btn_grp_01.setObjectName("layout_btn_grp_01")
-        self.btn_load = QtWidgets.QPushButton(self.verticalLayoutWidget_2)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setSpacing(0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.headWidget = QtWidgets.QWidget(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.headWidget.sizePolicy().hasHeightForWidth())
+        self.headWidget.setSizePolicy(sizePolicy)
+        self.headWidget.setMinimumSize(QtCore.QSize(0, 200))
+        self.headWidget.setObjectName("headWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.headWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.headLayout = QtWidgets.QHBoxLayout()
+        self.headLayout.setSpacing(10)
+        self.headLayout.setObjectName("headLayout")
+        self.logoLayout = QtWidgets.QVBoxLayout()
+        self.logoLayout.setContentsMargins(10, 10, -1, -1)
+        self.logoLayout.setObjectName("logoLayout")
+        self.logo = QtWidgets.QLabel(self.headWidget)
+        self.logo.setText("")
+        self.logo.setObjectName("logo")
+        self.logoLayout.addWidget(self.logo)
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.logoLayout.addItem(spacerItem)
+        self.headLayout.addLayout(self.logoLayout)
+        self.groupBox_imexport = QtWidgets.QGroupBox(self.headWidget)
+        self.groupBox_imexport.setMaximumSize(QtCore.QSize(200, 16777215))
+        self.groupBox_imexport.setObjectName("groupBox_imexport")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.groupBox_imexport)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.imexportLayout = QtWidgets.QVBoxLayout()
+        self.imexportLayout.setContentsMargins(7, -1, 7, -1)
+        self.imexportLayout.setSpacing(0)
+        self.imexportLayout.setObjectName("imexportLayout")
+        self.btn_load = QtWidgets.QPushButton(self.groupBox_imexport)
+        self.btn_load.setMaximumSize(QtCore.QSize(160, 16777215))
         self.btn_load.setObjectName("btn_load")
-        self.layout_btn_grp_01.addWidget(self.btn_load)
-        self.btn_save = QtWidgets.QPushButton(self.verticalLayoutWidget_2)
+        self.imexportLayout.addWidget(self.btn_load)
+        self.btn_save = QtWidgets.QPushButton(self.groupBox_imexport)
+        self.btn_save.setMaximumSize(QtCore.QSize(160, 16777215))
         self.btn_save.setObjectName("btn_save")
-        self.layout_btn_grp_01.addWidget(self.btn_save)
-        self.groupBox_2 = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox_2.setGeometry(QtCore.QRect(190, 10, 271, 80))
-        self.groupBox_2.setObjectName("groupBox_2")
-        self.gridLayoutWidget = QtWidgets.QWidget(self.groupBox_2)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(10, 10, 251, 71))
-        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
-        self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.gridLayout.setObjectName("gridLayout")
-        self.btn_compare = QtWidgets.QPushButton(self.gridLayoutWidget)
-        self.btn_compare.setObjectName("btn_compare")
-        self.gridLayout.addWidget(self.btn_compare, 0, 0, 1, 1)
-        self.combo_viewmode_2 = QtWidgets.QComboBox(self.gridLayoutWidget)
-        self.combo_viewmode_2.setObjectName("combo_viewmode_2")
-        self.combo_viewmode_2.addItem("")
-        self.combo_viewmode_2.addItem("")
-        self.combo_viewmode_2.addItem("")
-        self.gridLayout.addWidget(self.combo_viewmode_2, 0, 1, 1, 1)
+        self.imexportLayout.addWidget(self.btn_save)
+        self.horizontalLayout_2.addLayout(self.imexportLayout)
+        self.headLayout.addWidget(self.groupBox_imexport)
+        self.groupBox_Evaluate = QtWidgets.QGroupBox(self.headWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.groupBox_Evaluate.sizePolicy().hasHeightForWidth())
+        self.groupBox_Evaluate.setSizePolicy(sizePolicy)
+        self.groupBox_Evaluate.setMaximumSize(QtCore.QSize(200, 16777215))
+        self.groupBox_Evaluate.setObjectName("groupBox_Evaluate")
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout(self.groupBox_Evaluate)
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.compareLayout = QtWidgets.QVBoxLayout()
+        self.compareLayout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
+        self.compareLayout.setContentsMargins(7, -1, 7, -1)
+        self.compareLayout.setSpacing(0)
+        self.compareLayout.setObjectName("compareLayout")
+        self.pushButton = QtWidgets.QPushButton(self.groupBox_Evaluate)
+        self.pushButton.setEnabled(False)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setSizeIncrement(QtCore.QSize(0, 0))
+        self.pushButton.setObjectName("pushButton")
+        self.compareLayout.addWidget(self.pushButton)
+        self.comboBox = QtWidgets.QComboBox(self.groupBox_Evaluate)
+        self.comboBox.setObjectName("comboBox")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.compareLayout.addWidget(self.comboBox)
+        self.horizontalLayout_3.addLayout(self.compareLayout)
+        self.headLayout.addWidget(self.groupBox_Evaluate)
+        self.groupBox_Tools = QtWidgets.QGroupBox(self.headWidget)
+        self.groupBox_Tools.setMaximumSize(QtCore.QSize(300, 16777215))
+        self.groupBox_Tools.setObjectName("groupBox_Tools")
+        self.verticalLayout_5 = QtWidgets.QVBoxLayout(self.groupBox_Tools)
+        self.verticalLayout_5.setSpacing(7)
+        self.verticalLayout_5.setObjectName("verticalLayout_5")
+        self.verticalLayout_Tools = QtWidgets.QVBoxLayout()
+        self.verticalLayout_Tools.setContentsMargins(7, -1, 7, -1)
+        self.verticalLayout_Tools.setSpacing(0)
+        self.verticalLayout_Tools.setObjectName("verticalLayout_Tools")
+        self.btn_tool_cursor = QtWidgets.QPushButton(self.groupBox_Tools)
+        self.btn_tool_cursor.setCheckable(True)
+        self.btn_tool_cursor.setChecked(False)
+        self.btn_tool_cursor.setFlat(False)
+        self.btn_tool_cursor.setObjectName("btn_tool_cursor")
+        self.verticalLayout_Tools.addWidget(self.btn_tool_cursor)
+        self.btn_tool_edit = QtWidgets.QPushButton(self.groupBox_Tools)
+        self.btn_tool_edit.setCheckable(True)
+        self.btn_tool_edit.setObjectName("btn_tool_edit")
+        self.verticalLayout_Tools.addWidget(self.btn_tool_edit)
+        self.verticalLayout_5.addLayout(self.verticalLayout_Tools)
+        self.headLayout.addWidget(self.groupBox_Tools)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.headLayout.addItem(spacerItem1)
+        self.headLayout.setStretch(1, 1)
+        self.headLayout.setStretch(2, 1)
+        self.headLayout.setStretch(3, 1)
+        self.headLayout.setStretch(4, 1)
+        self.horizontalLayout.addLayout(self.headLayout)
+        self.verticalLayout_2.addWidget(self.headWidget)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.mplWidget = QtWidgets.QWidget(self.centralwidget)
+        self.mplWidget.setStyleSheet("background-color: white")
+        self.mplWidget.setObjectName("mplWidget")
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.mplWidget)
+        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_4.setSpacing(0)
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        self.mplLayout = QtWidgets.QVBoxLayout()
+        self.mplLayout.setSpacing(0)
+        self.mplLayout.setObjectName("mplLayout")
+        self.verticalLayout_4.addLayout(self.mplLayout)
+        self.verticalLayout.addWidget(self.mplWidget)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1392, 18))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1517, 26))
         self.menubar.setObjectName("menubar")
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
@@ -79,13 +225,16 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.groupBox.setTitle(_translate("MainWindow", "CSV"))
-        self.btn_load.setText(_translate("MainWindow", "Laden"))
-        self.btn_save.setText(_translate("MainWindow", "Speichern"))
-        self.groupBox_2.setTitle(_translate("MainWindow", "Auswerten"))
-        self.btn_compare.setText(_translate("MainWindow", "Verleiche Soll-Ist"))
-        self.combo_viewmode_2.setItemText(0, _translate("MainWindow", "Überlappend"))
-        self.combo_viewmode_2.setItemText(1, _translate("MainWindow", "Untereinander"))
-        self.combo_viewmode_2.setItemText(2, _translate("MainWindow", "Nebeneinander"))
+        self.groupBox_imexport.setTitle(_translate("MainWindow", "Import/Export"))
+        self.btn_load.setText(_translate("MainWindow", "CSV Importieren"))
+        self.btn_save.setText(_translate("MainWindow", "CSV Exportieren"))
+        self.groupBox_Evaluate.setTitle(_translate("MainWindow", "Auswerten"))
+        self.pushButton.setText(_translate("MainWindow", "Vergleiche Soll-Ist"))
+        self.comboBox.setItemText(0, _translate("MainWindow", "Überlappend"))
+        self.comboBox.setItemText(1, _translate("MainWindow", "Untereinander"))
+        self.comboBox.setItemText(2, _translate("MainWindow", "Übereinander"))
+        self.groupBox_Tools.setTitle(_translate("MainWindow", "Werkzeuge"))
+        self.btn_tool_cursor.setText(_translate("MainWindow", "Cursor"))
+        self.btn_tool_edit.setText(_translate("MainWindow", "Bearbeiten"))
         self.menuFile.setTitle(_translate("MainWindow", "File"))
         self.actionExit.setText(_translate("MainWindow", "Exit"))
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from matplotlib.figure import Figure
 from configparser import ConfigParser
 from PyQt5 import QtCore
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout, QGroupBox, QWidget, QVBoxLayout, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout, QGroupBox, QWidget, QVBoxLayout, QFileDialog, QHBoxLayout, QFrame, QSplitter, QStyleFactory
 from MainWindow import Ui_MainWindow
 from PyLcSnap7 import S7Conn, Smarttags
 
@@ -22,7 +22,7 @@ class MplCanvas(FigureCanvasQTAgg):
         fig.subplots_adjust(
             top=0.93,
             bottom=0.065,
-            left=0.03,
+            left=0.05,
             right=0.989,
             hspace=0.2,
             wspace=0.2
@@ -78,12 +78,19 @@ class PLC(QtCore.QThread):
 class GraphMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, plc, config, *args, **kwargs):
         super(GraphMainWindow, self).__init__(*args, **kwargs)
-        self.setupUi(self)
-        self.connect_componets()
         self.cfg = config
         self.plc = PLC(self)
         self.plc.start()
-        self.setWindowIcon(QtGui.QIcon('./assets/primary-chart-line.png'))
+        self.setupUi(self)
+        self.setupWindow()
+        self.connect_componets()
+
+
+
+
+        QApplication.setStyle(QStyleFactory.create('Cleanlooks'))
+
+
 
         self.graph = MplCanvas(self, width=5, height=4, dpi=100)
         self.graph.plot_current([0,1,2,3,4], [10,1,20,3,40])
@@ -97,27 +104,50 @@ class GraphMainWindow(QMainWindow, Ui_MainWindow):
 
         self.show()
 
+    def setupWindow(self):
+        self.setWindowIcon(QtGui.QIcon('./assets/primary-chart-line.png'))
+        self.setWindowTitle(self.cfg['GUI']['title'])
+        self.logo.setPixmap(QtGui.QPixmap('./assets/logo.png'))
+
     def connect_componets(self):
         self.btn_load.clicked.connect(self.handle_btn_load)
         self.btn_save.clicked.connect(self.handle_btn_save)
-        self.btn_compare.clicked.connect(self.handle_btn_compare)
+        #self.btn_compare.clicked.connect(self.handle_btn_compare)
+        self.btn_tool_cursor.clicked.connect(self.handle_btn_tool_cursor)
+        self.btn_tool_edit.clicked.connect(self.handle_btn_tool_edit)
 
     def handle_btn_load(self):
         options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, fileType = QFileDialog.getOpenFileName(self,"CSV Laden","","CSV (*.csv)", options=options)
+        if not self.cfg['GUI'].getboolean('use_native_filedialog'):
+            options |= QFileDialog.DontUseNativeDialog
+        fileName, fileType = QFileDialog.getOpenFileName(self,self.cfg['STRINGS']['csv_load_title'],"","CSV (*.csv)", options=options)
         if fileName:
             print(fileName+'.csv')
 
     def handle_btn_save(self):
         options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, fileType = QFileDialog.getSaveFileName(self,"CSV Speichern","","CSV (*.csv)", options=options)
+        if not self.cfg['GUI'].getboolean('use_native_filedialog'):
+            options |= QFileDialog.DontUseNativeDialog
+        fileName, fileType = QFileDialog.getSaveFileName(self,self.cfg['STRINGS']['csv_save_title'],"","CSV (*.csv)", options=options)
         if fileName:
             print(fileName+'.csv')
 
     def handle_btn_compare(self):
         print('compare')
+
+    def handle_btn_tool_cursor(self):
+        if self.btn_tool_cursor.isChecked():
+            self.btn_tool_edit.setChecked(False)
+            print('Cursor On')
+        else:
+            print('Cursor Off')
+
+    def handle_btn_tool_edit(self):
+        if self.btn_tool_edit.isChecked():
+            self.btn_tool_cursor.setChecked(False)
+            print('Edit On')
+        else:
+            print('Edit Off')
 
 
 if __name__ == '__main__':
@@ -126,4 +156,4 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     plc = S7Conn(config['PLC']['ip'])
     gui = GraphMainWindow(plc, config)
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
